27db1b460bb4e04e911105158f9daa5773bd645f
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-14 19:15:17 -0600

    Update No Will to Break.cpp

--- No Will to Break.cpp
+++ No Will to Break.cpp
@@ -12,10 +12,34 @@ void displayArray(int numbers[]){
  cout << endl;
 }
 
+void displayBackwards(int numbers[]){
+ for(int i = SIZE - 1; i >= 0; i--){
+   cout << numbers[i] << ", ";
+ }
+ cout << endl;
+}
+
+void sum(int numbers[]) {
+  int end = 0;
+  for (int i = 0; i < SIZE; i++) {
+    end += numbers[i];
+  }
+  cout << end << endl;
+}
+
+void swap(int numbers[]) {
+  cout << numbers[SIZE - 1] << ", ";
+  for(int i = 1; i < SIZE - 1; i++){
+   cout << numbers[i] << ", ";
+ }
+ cout << numbers[0] << endl;
+}
 
 int main() {
    int numbers[SIZE] = {5, 9, 3, 4, 5, 3, 2, 1, 4, 7};
    displayArray(numbers);
-
+   displayBackwards(numbers);
+   sum(numbers);
+   swap(numbers);
   return 0;
 }
